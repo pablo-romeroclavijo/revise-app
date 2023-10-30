64408891e57cdfc407c567361989afa9f1fc8647
Tayeeb Islam <54636425+Tayeeb27@users.noreply.github.com>
2023-10-30 16:56:39 +0000

    Added UseEffect

--- client/src/components/Timetable/index.jsx
+++ client/src/components/Timetable/index.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { EventCard } from '../index';
+import React, { useState, useEffect, useRef } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import daygridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
@@ -8,6 +7,35 @@ import { v4 as uuid } from 'uuid';
 
 export default function Timetable() {
   const [events, setEvents] = useState([]);
+  const calendarRef = useRef(null); // Declare the calendar reference
+
+  // A useEffect to set up event handling after the initial render.
+  useEffect(() => {
+    // Event handling for when an event is dropped or resized.
+    const handleEventChange = (eventInfo) => {
+      const updatedEvents = events.map((event) => {
+        if (event.id === eventInfo.event.id) {
+          return {
+            ...event,
+            start: eventInfo.event.start,
+            end: eventInfo.event.end,
+          };
+        }
+        return event;
+      });
+      setEvents(updatedEvents);
+    };
+
+    // Add the event handlers for event drop and resize.
+    const calendarApi = calendarRef.current.getApi();
+    calendarApi.on('eventDrop', handleEventChange);
+    calendarApi.on('eventResize', handleEventChange);
+
+    return () => {
+      calendarApi.off('eventDrop', handleEventChange);
+      calendarApi.off('eventResize', handleEventChange);
+    };
+  }, [events]);
 
   const handleSelect = (info) => {
     const { start } = info;
@@ -32,21 +60,19 @@ export default function Timetable() {
       }
     }
   };
-
-  console.log(events);
-
+  console.log(events)
   return (
     <div>
       <FullCalendar
         editable
         selectable
+        ref={calendarRef}
         events={events}
         select={handleSelect}
         headerToolbar={{
           start: 'today prev next',
           end: 'dayGridMonth timeGridWeek,timeGridDay',
         }}
-        eventContent={(info) => <EventCard info={info} />}
         plugins={[daygridPlugin, interactionPlugin, timeGridPlugin]}
         views={['dayGridMonth', 'timeGridWeek', 'timeGridDay']}
       />
