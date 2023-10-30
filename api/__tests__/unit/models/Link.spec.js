cc677d9f8dbb12bac44a501c151ca5481d6ff08b
mgr-mirnal <mgr.mrinal97@gmail.com>
2023-10-30 13:47:35 +0000

    made template for test

---
+++ api/__tests__/unit/models/Link.spec.js
@@ -0,0 +1,26 @@
+const Link = require('../../../models/Link')
+const db = require('../../../database/connect')
+
+describe('Link',() => {
+    describe('',() => {
+
+    
+    })
+
+    describe('', () => {
+
+    })
+      
+    describe('', ()=> {
+
+    })
+
+    describe('', ()=> {
+
+    })
+    
+    describe('', ()=>{
+
+    })
+
+})
