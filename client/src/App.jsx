d541f3fcb994c99bb3aaa01d4fc8d514dc1298c8
Tayeeb Islam <54636425+Tayeeb27@users.noreply.github.com>
2023-10-31 09:07:52 +0000

    Added Start and End for Month

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./App.css";
 import { Routes, Route } from 'react-router-dom';
 import {Timetable} from './components';
 const App = () => {
-    
+  const [events, setEvents] = useState([]);
   return(
   <>
-    <Timetable />
+    <Timetable events={events} setEvents={setEvents} />
   </>
   )
 };
--- client/src/components/Timetable/index.jsx
+++ client/src/components/Timetable/index.jsx
@@ -1,17 +1,26 @@
 import React, { useState, useEffect, useRef } from 'react';
 import FullCalendar from '@fullcalendar/react';
-import daygridPlugin from '@fullcalendar/daygrid';
+import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import { v4 as uuid } from 'uuid';
 
-export default function Timetable() {
-  const [events, setEvents] = useState([]);
-  const calendarRef = useRef(null); // Declare the calendar reference
+export default function Timetable({ events, setEvents }) {
+  const calendarRef = useRef(null);
 
-  // A useEffect to set up event handling after the initial render.
+  const handleSubmit = (eventName, start, end) => {
+    setEvents([
+      ...events,
+      {
+        start,
+        end,
+        title: eventName,
+        id: uuid(),
+      },
+    ]);
+  };
+
   useEffect(() => {
-    // Event handling for when an event is dropped or resized.
     const handleEventChange = (eventInfo) => {
       const updatedEvents = events.map((event) => {
         if (event.id === eventInfo.event.id) {
@@ -26,41 +35,66 @@ export default function Timetable() {
       setEvents(updatedEvents);
     };
 
-    // Add the event handlers for event drop and resize.
+    const handleViewChange = (viewInfo) => {
+      const currentView = viewInfo.view.type;
+
+      if (currentView === 'dayGridMonth') {
+        
+        const updatedEvents = events.filter((event) => {
+          return (
+            event.start >= viewInfo.start && event.end <= viewInfo.end
+          );
+        });
+
+        setEvents(updatedEvents);
+      }
+    };
+
     const calendarApi = calendarRef.current.getApi();
     calendarApi.on('eventDrop', handleEventChange);
     calendarApi.on('eventResize', handleEventChange);
-
-    return () => {
-      calendarApi.off('eventDrop', handleEventChange);
-      calendarApi.off('eventResize', handleEventChange);
-    };
+    calendarApi.on('viewDidMount', handleViewChange);
   }, [events]);
 
   const handleSelect = (info) => {
     const { start } = info;
     const eventNamePrompt = prompt('Enter event name:');
     if (eventNamePrompt) {
-      const durationPrompt = prompt('Enter event duration (HH:mm):');
-      if (durationPrompt) {
-        const [hours, minutes] = durationPrompt.split(':');
-        const duration = {
-          hours: parseInt(hours),
-          minutes: parseInt(minutes),
-        };
-        setEvents([
-          ...events,
-          {
-            start,
-            end: new Date(start.getTime() + duration.hours * 60 * 60 * 1000 + duration.minutes * 60 * 1000),
-            title: eventNamePrompt,
-            id: uuid(),
-          },
-        ]);
+      if (info.view.type === 'dayGridMonth') {
+        const startTimePrompt = prompt('Enter start time (HH:mm):');
+        const endTimePrompt = prompt('Enter end time (HH:mm):');
+
+        if (startTimePrompt && endTimePrompt) {
+          const [startHours, startMinutes] = startTimePrompt.split(':');
+          const [endHours, endMinutes] = endTimePrompt.split(':');
+          const newStart = new Date(start);
+          newStart.setHours(parseInt(startHours));
+          newStart.setMinutes(parseInt(startMinutes));
+          const newEnd = new Date(start);
+          newEnd.setHours(parseInt(endHours));
+          newEnd.setMinutes(parseInt(endMinutes));
+
+          handleSubmit(eventNamePrompt, newStart, newEnd);
+        }
+      } else {
+        
+        const durationPrompt = prompt('Enter event duration (HH:mm):');
+
+        if (durationPrompt) {
+          const [hours, minutes] = durationPrompt.split(':');
+          const duration = {
+            hours: parseInt(hours),
+            minutes: parseInt(minutes),
+          };
+
+          const newEnd = new Date(start.getTime() + duration.hours * 60 * 60 * 1000 + duration.minutes * 60 * 1000);
+
+          handleSubmit(eventNamePrompt, start, newEnd);
+        }
       }
     }
   };
-  console.log(events)
+
   return (
     <div>
       <FullCalendar
@@ -73,7 +107,7 @@ export default function Timetable() {
           start: 'today prev next',
           end: 'dayGridMonth timeGridWeek,timeGridDay',
         }}
-        plugins={[daygridPlugin, interactionPlugin, timeGridPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
         views={['dayGridMonth', 'timeGridWeek', 'timeGridDay']}
       />
     </div>
