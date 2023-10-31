82f0e8bebcf51841beed6e004c7daf2eeed181d3
Tayeeb Islam <54636425+Tayeeb27@users.noreply.github.com>
2023-10-31 19:35:10 +0000

    Uses DB for Fetch, Post, Delete Edit still needs work

--- client/src/components/EventCard/index.jsx
+++ client/src/components/EventCard/index.jsx
@@ -1,63 +1,80 @@
 import React, { useState } from 'react';
 
-export default function EventCard({ info, events, setEvents }) {
-  const { event } = info;
-  const { _instance } = event;
-  const { range } = _instance;
-
+const EventCard = ({ eventInfo, onDelete, onEdit }) => {
+  console.log(eventInfo.extendedProps) 
+  
   const [isEditing, setIsEditing] = useState(false);
-  const [editedEvent, setEditedEvent] = useState({});
+  const [editedEvent, setEditedEvent] = useState({ ...eventInfo.extendedProps });
+  const start = new Date(eventInfo.extendedProps.start_date);
+  const end = new Date(eventInfo.extendedProps.end_date);
 
-  const handleEditEvent = () => {
+  const handleEdit = () => {
     setIsEditing(true);
-    setEditedEvent(event); 
   };
 
-  const handleSaveEdit = () => {
-    setIsEditing(false);
-    
-    const updatedEvents = events.map((e) =>
-      e.id === editedEvent.id ? editedEvent : e
-      
-    );
-    setEvents(updatedEvents);
-  };
-
-  const handleCancelEdit = () => {
+  const handleSave = () => {
+    // You can add validation here to ensure data is correctly formatted
+    onEdit(editedEvent);
     setIsEditing(false);
   };
 
-  const handleDeleteEvent = () => {
-    const updatedEvents = events.filter((e) => e.publicId !== event.publicId);
-    setEvents(updatedEvents);
+  const handleDelete = () => {
+    onDelete(eventInfo.extendedProps.event_id);
   };
 
-  if (isEditing) {
-    return (
-      <div>
-        <input
-          type="text"
-          value={editedEvent.title}
-          onChange={(e) =>
-            setEditedEvent({ ...editedEvent, title: e.target.value })
-          }
-        />
-        <button onClick={handleSaveEdit}>Save</button>
-        <button onClick={handleCancelEdit}>Cancel</button>
-      </div>
-    );
-  } else if (range) {
-    const eventStart = range.start.toLocaleTimeString();
-    const eventEnd = range.end.toLocaleTimeString();
-    console.log(events)
-    return (
-      <div>
-        <p>
-          {eventStart} - {eventEnd} - {event.title}
-        </p>
-        <button onClick={handleEditEvent}>Edit</button>
-        <button onClick={handleDeleteEvent}>Delete</button>
-      </div>
-    );
-  } 
-}
+  return (
+    <div className='event-card'>
+      {isEditing ? (
+        <div>
+          <h3>Edit Event</h3>
+          <input
+            type='text'
+            placeholder='Event Title'
+            value={editedEvent.title}
+            onChange={(e) => setEditedEvent({ ...editedEvent, title: e.target.value })}
+          />
+          <input
+            type='text'
+            placeholder='Subject'
+            value={editedEvent.subject}
+            onChange={(e) => setEditedEvent({ ...editedEvent, subject: e.target.value })}
+          />
+          <input
+            type='text'
+            placeholder='Description'
+            value={editedEvent.description}
+            onChange={(e) => setEditedEvent({ ...editedEvent, description: e.target.value })}
+          />
+          <input
+            type='text'
+            placeholder='Location'
+            value={editedEvent.location}
+            onChange={(e) => setEditedEvent({ ...editedEvent, location: e.target.value })}
+          />
+          <input
+            type='text'
+            placeholder='Priority'
+            value={editedEvent.priority}
+            onChange={(e) => setEditedEvent({ ...editedEvent, priority: e.target.value })}
+          />
+          <button onClick={handleSave}>Save</button>
+        </div>
+      ) : (
+        <div>
+          <h3>{eventInfo.extendedProps.title}</h3>
+          <p>Subject: {eventInfo.extendedProps.subject}</p>
+          <p>Description: {eventInfo.extendedProps.description}</p>
+          <p>Location: {eventInfo.extendedProps.location}</p>
+          <p>Priority: {eventInfo.extendedProps.priority}</p>
+          <p>Start: {start.toLocaleString()}</p>
+          <p>End: {end.toLocaleString()}</p>
+          <button onClick={handleEdit}>Edit</button>
+          <button onClick={handleDelete}>Delete</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default EventCard;
+
