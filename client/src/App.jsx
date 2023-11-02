0e0c8c77b66a9ce3fa5bde38f019de7758390a22
Tayeeb Islam <54636425+Tayeeb27@users.noreply.github.com>
2023-11-02 11:06:55 +0000

    Login and SignUp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import { Routes, Route } from 'react-router-dom';
-import {TimetablePage, LoginPage, NotFoundPage} from './pages'
+import {TimetablePage, LoginPage, NotFoundPage, SignUpPage} from './pages'
 const App = () => {
   return(
   <>
@@ -9,6 +9,7 @@ const App = () => {
     <Routes>
       <Route path="/">
         <Route path="/" element={<LoginPage />}/>
+        <Route path="/signup" element={<SignUpPage />}/>
         <Route path="/timetable" element={<TimetablePage />}/>
         <Route path="*" element={<NotFoundPage />} />
       </Route>
