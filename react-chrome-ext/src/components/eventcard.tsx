f287f6007a9eba3f9d6283c9020066392555657b
pablo-romeroclavijo <136720541+pablo-romeroclavijo@users.noreply.github.com>
2023-11-02 14:27:26 +0000

    sdf

--- react-chrome-ext/src/components/eventcard.tsx
+++ react-chrome-ext/src/components/eventcard.tsx
@@ -1,6 +1,7 @@
 
 import React, {ReactNode} from "react"
 
+
 type EventProps = {
   event : any
 }
@@ -9,23 +10,34 @@ type EventProps = {
 const EventCard  : React.FunctionComponent<EventProps> = (props) => {
 
 const{event} = props
-console.log(typeof(event))
 
 const {title, start_date, end_date, subject} = event
 
+const today = new Date()
+const eventDate = new Date(event.start_date)
+console.log('----------')
+console.log(eventDate)
+console.log(today)
 
 
-return (
 
-<div className='event-card'>
-  <h3>{title}</h3>
-  <p>{subject}</p>
+const body = (
+  <div className='event-card'>
+  <div className="row">
+    <h3>{title}</h3>
+    <p>{subject}</p>
+  </div>
   <p>{start_date}</p>
   <p>{end_date}</p>
 
   <button>Completed</button>
   <br></br>
 </div>
+)
+
+return (body
+
+
 )
 }
 export default EventCard
